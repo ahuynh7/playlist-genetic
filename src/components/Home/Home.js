c849000e65c444a562cbf3bd4823f7a147127347
Anh Huynh <huynhanh8327@gmail.com>
2022-04-19 16:59:30 -0700

    fixed .env

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,23 @@
+import { useEffect } from "react";
+import { Button } from "react-bootstrap";
+
 const Home = () => {
+    const requestAuth = () => {
+        const AUTHORIZE = 'https://accounts.spotify.com/authorize';
+        console.log(process.env.REACT_APP_REDIRECT_URI);
+    };
+
+    useEffect(() => {
+        requestAuth();
+    }, []);
+
     return (
-        <h1>home</h1>
+        <>
+            <h1>home</h1>
+            <Button size='sm'>
+                log in
+            </Button>
+        </>
     );
 };
 
