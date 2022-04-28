3dd77f47bd62adcc92ef60d280c3342cb3542f6c
Anh Huynh <huynhanh8327@gmail.com>
2022-04-28 13:54:50 -0700

    top tracks frequency graph prototype

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,5 @@
-import { createContext } from "react";
-import { Table } from "react-bootstrap";
+import { createContext, useEffect, useRef } from "react";
+import { BarChart, Bar, XAxis, YAxis } from "recharts";
 import { useUserFetch, useUserPlaylistFetch, useUserTopArtistFetch, useUserTopTrackFetch } from "../../hooks/useUser";
 
 export const UserContext = createContext();
@@ -16,26 +16,31 @@ const Main = () => {
         topTracks,
         topArtists
     };
-    
+
+    const map = useRef({});
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            for (let i = 0; i < 100; i++) {
+                map.current[i] = 0
+            }
+            topTracks.forEach(item => {
+                map.current[item.popularity.toString()]++;
+            });
+
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [topTracks]);
+
     return (
         <UserContext.Provider value={contextPackage}>
             <div>welcome {user.display_name}</div>
-            <Table>
-                <thead>
-                    <tr>
-                        <th>name</th>
-                        <th>description</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.values(playlists).map((playlist, i) => 
-                        <tr key={i}>
-                            <td>{playlist.name}</td>
-                            <td>{playlist.description}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
+            <BarChart width={932} height={400} data={Object.keys(map.current).map(e => ({name: e, freq: map.current[e]}))}>
+                <XAxis dataKey='name' />
+                <YAxis />
+                <Bar dataKey='freq' fill='#1db954'/>
+            </BarChart>
         </UserContext.Provider>
     );
 };
