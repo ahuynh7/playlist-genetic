fbfbf80e01fed3e80166ba107efc4456905c9392
Anh Huynh <huynhanh8327@gmail.com>
2022-05-03 11:51:33 -0700

    functioning feature picker

--- src/redux/sagas/userSaga.js
+++ src/redux/sagas/userSaga.js
@@ -1,13 +1,22 @@
-import { delay, put, take } from 'redux-saga/effects';
+import { delay, put, take, takeEvery } from 'redux-saga/effects';
 
 import { getUser, getUserTopArtists, getUserTopTracks } from '../slices/userSlice';
 import { requestAccessToken } from '../slices/authorizationSlice';
 
-//handle 503 errors?
+function* retryGetUserTop(thunk, {meta, payload}) {
+    if (payload.error.status !== 503) return;
+
+    yield delay(2000);      //custom retry time
+    yield put(thunk(meta.arg));
+}
 
 function* userSaga() {
     //once user data has been fetched, dispatch thunks to get playlist, artists, track data
     yield take(requestAccessToken.fulfilled);
+
+    //handle 503 errors
+    yield takeEvery(getUserTopTracks.rejected, retryGetUserTop, getUserTopTracks);
+    yield takeEvery(getUserTopArtists.rejected, retryGetUserTop, getUserTopArtists);
 
     const terms = ['short_term', 'medium_term', 'long_term'];
 
