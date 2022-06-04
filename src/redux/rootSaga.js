021a342672bec59d0cf5a785a987c8051cb3cb69
Anh Huynh <huynhanh8327@gmail.com>
2022-06-03 23:55:01 -0700

    disabled getting top items

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,13 +1,13 @@
 import { all, fork } from "redux-saga/effects";
 
 import playlistSaga from "./sagas/playlistSaga";
-import topSaga from "./sagas/topSaga";
+//import topSaga from "./sagas/topSaga";
 import userSaga from "./sagas/userSaga";
 
 function* rootSaga() {
     yield all([
         fork(userSaga),
-        fork(topSaga),
+        //fork(topSaga),        //temporarily disabled to be granted quota extension request
         fork(playlistSaga)
     ]);
 }
