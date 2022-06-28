bfb951617007678f06da884623871afc77da05d3
Anh Huynh <huynhanh8327@gmail.com>
2022-06-28 09:59:10 -0700

    buymeacoffee link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,17 @@
+import { useLocation } from "react-router-dom";
 import { FooterLink, FooterWrapper } from "./Footer.styles";
 
 const Footer = () => {
+    const {pathname} = useLocation();
+
     return (
         <FooterWrapper id="footer">
             <FooterLink to="/about">About</FooterLink>
-            <FooterLink to="">Donate!</FooterLink>
+            <FooterLink to={pathname}
+                onClick={() => window.open("https://www.buymeacoffee.com/19ahuynh")}
+            >
+                Donate!
+            </FooterLink>
             <FooterLink to="/faq">FAQ</FooterLink>
         </FooterWrapper>
     );
